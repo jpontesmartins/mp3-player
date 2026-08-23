0c433a4b39f4899490eac2675b62677d3cd6ca10
= <jpontesmartins@gmail.com>
2026-08-23 10:49:01 -0300

    correção no download da letra

--- backend/src/test/java/com/mp3player/lyrics/infrastructure/JsoupLyricsScraperTest.java
+++ backend/src/test/java/com/mp3player/lyrics/infrastructure/JsoupLyricsScraperTest.java
@@ -61,8 +61,10 @@ class JsoupLyricsScraperTest {
 
     @Test
     void fetchReturnsLyricsFromDirectUrl() throws IOException {
+        String artistPageUrl = "https://www.letras.mus.br/legião-urbana/";
         String directUrl = "https://www.letras.mus.br/legião-urbana/tempo-perdido/";
         Map<String, Document> docs = new HashMap<>();
+        docs.put(artistPageUrl, Jsoup.parse("<html><body></body></html>"));
         docs.put(directUrl, Jsoup.parse(lyricHtml()));
         Map<String, Integer> statuses = new HashMap<>();
         statuses.put(directUrl, 200);
@@ -75,15 +77,18 @@ class JsoupLyricsScraperTest {
 
     @Test
     void fetchFallsBackToSearchAndStripsTranslationSuffix() throws IOException {
+        String artistPageUrl = "https://www.letras.mus.br/legião-urbana/";
         String searchUrl = "https://www.letras.mus.br/?q="
                 + java.net.URLEncoder.encode("Legião Urbana Tempo Perdido", java.nio.charset.StandardCharsets.UTF_8);
         String translatedUrl = "https://www.letras.mus.br/legião-urbana/tempo-perdido-traducao.html";
         String strippedUrl = translatedUrl.substring(0, translatedUrl.length() - "traducao.html".length());
 
+        Document artistDoc = Jsoup.parse("<html><body></body></html>");
         Document searchDoc = Jsoup.parse("<html><body><a class=\"gs-title\" href=\"" + translatedUrl + "\">link</a></body></html>");
         Document lyricsDoc = Jsoup.parse(lyricHtml());
 
         Map<String, Document> docs = new HashMap<>();
+        docs.put(artistPageUrl, artistDoc);
         docs.put(searchUrl, searchDoc);
         docs.put(strippedUrl, lyricsDoc);
         Map<String, Integer> statuses = new HashMap<>();
@@ -142,6 +147,72 @@ class JsoupLyricsScraperTest {
             assertTrue(text.contains("Artista"));
         } finally {
             jsoup.close();
+        }
+    }
+
+    @Test
+    void artistPageFindsExactMatch() throws IOException {
+        String artistPageUrl = "https://www.letras.mus.br/james-blunt/";
+        String lyricsUrl = "https://www.letras.mus.br/james-blunt/no-tears/";
+        String artistPageHtml = "<html><body>"
+                + "<a href=\"/james-blunt/tears-and-rain/\">Tears And Rain</a>"
+                + "<a href=\"/james-blunt/no-tears/\">No Tears</a>"
+                + "</body></html>";
+        Document artistDoc = Jsoup.parse(artistPageHtml);
+        Document lyricsDoc = Jsoup.parse(lyricHtml());
+
+        Map<String, Document> docs = new HashMap<>();
+        docs.put(artistPageUrl, artistDoc);
+        docs.put(lyricsUrl, lyricsDoc);
+        Map<String, Integer> statuses = new HashMap<>();
+        statuses.put(lyricsUrl, 200);
+
+        try (MockedStatic<Jsoup> jsoup = mockJsoup(docs, statuses)) {
+            String text = new JsoupLyricsScraper(DEFAULT_PROPS).fetch("James Blunt", "No Tears");
+            assertEquals(lyricText(), text);
+        }
+    }
+
+    @Test
+    void artistPageRejectsPartialMatch() throws IOException {
+        String artistPageUrl = "https://www.letras.mus.br/radiohead/";
+        String artistPageHtml = "<html><body>"
+                + "<a href=\"/radiohead/you-and-whose-army/\">You And Whose Army</a>"
+                + "<a href=\"/radiohead/just/\">Just</a>"
+                + "</body></html>";
+        Document artistDoc = Jsoup.parse(artistPageHtml);
+
+        Map<String, Document> docs = new HashMap<>();
+        docs.put(artistPageUrl, artistDoc);
+        Map<String, Integer> statuses = new HashMap<>();
+
+        try (MockedStatic<Jsoup> jsoup = mockJsoup(docs, statuses)) {
+            String text = new JsoupLyricsScraper(DEFAULT_PROPS).fetch("Radiohead", "You");
+            assertTrue(text.startsWith("Letra não encontrada"));
+            assertTrue(text.contains("You"));
+        }
+    }
+
+    @Test
+    void searchDoesNotMatchPartialTitle() throws IOException {
+        String searchUrl = "https://www.letras.mus.br/?q="
+                + java.net.URLEncoder.encode("Radiohead You", java.nio.charset.StandardCharsets.UTF_8);
+        String searchHtml = "<html><body>"
+                + "<a class=\"gs-title\" href=\"https://www.letras.mus.br/radiohead/you-and-whose-army/\">You And Whose Army</a>"
+                + "<a class=\"gs-title\" href=\"https://www.letras.mus.br/radiohead/you/\">You</a>"
+                + "</body></html>";
+        Document searchDoc = Jsoup.parse(searchHtml);
+        Document lyricsDoc = Jsoup.parse(lyricHtml());
+
+        Map<String, Document> docs = new HashMap<>();
+        docs.put(searchUrl, searchDoc);
+        docs.put("https://www.letras.mus.br/radiohead/you/", lyricsDoc);
+        Map<String, Integer> statuses = new HashMap<>();
+        statuses.put("https://www.letras.mus.br/radiohead/you/", 200);
+
+        try (MockedStatic<Jsoup> jsoup = mockJsoup(docs, statuses)) {
+            String text = new JsoupLyricsScraper(DEFAULT_PROPS).fetch("Radiohead", "You");
+            assertEquals(lyricText(), text);
         }
     }
 }
